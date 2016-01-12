da6b91ae03c925fc88af4efba4d99b26c037974a
shimesaba9 <hshs012@gmail.com>
2016-01-12 21:33:37 +0900

    Create SwiftDefaults.swift

---
+++ Example/SwiftDefaults/MyDefaults.swift
@@ -0,0 +1,13 @@
+//
+//  MyDefaults.swift
+//  SwiftDefaults
+//
+//  Created by 杉本裕樹 on 2016/01/12.
+//  Copyright © 2016年 CocoaPods. All rights reserved.
+//
+
+import Cocoa
+
+class MyDefaults: SwiftDefaults {
+
+}
---
+++ Pod/Classes/SwiftDefaults.swift
@@ -0,0 +1,42 @@
+class SwiftDefaults: NSObject {
+    let userDefaults = NSUserDefaults.standardUserDefaults()
+    
+    override init() {
+        super.init()
+        
+        registerDefaults()
+        setupProperty()
+        addObserver()
+    }
+    
+    private func registerDefaults() {
+        let dic = propertyNames.reduce([String:AnyObject]()) { (var dic, key) -> [String:AnyObject] in
+            dic[key] = valueForKey(key)
+            return dic
+        }
+        userDefaults.registerDefaults(dic)
+    }
+    
+    private func setupProperty() {
+        propertyNames.forEach {
+            setValue(userDefaults.objectForKey($0), forKey: $0)
+        }
+    }
+    
+    private func addObserver() {
+        propertyNames.forEach {
+            addObserver(self, forKeyPath: $0, options: .New, context: nil)
+        }
+    }
+    
+    private var propertyNames: [String] {
+        return Mirror(reflecting: self).children.flatMap { $0.label }
+    }
+    
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        
+        if let keyPath = keyPath {
+            userDefaults.setObject(change?["new"], forKey: keyPath)
+        }
+    }
+}
