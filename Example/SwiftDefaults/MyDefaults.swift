92bfce972702871ecbf98411b587f5740f00b930
shimesaba9 <hshs012@gmail.com>
2016-09-28 00:01:52 +0900

    Support Swift3.0

--- Example/SwiftDefaults/MyDefaults.swift
+++ Example/SwiftDefaults/MyDefaults.swift
@@ -14,5 +14,6 @@ class MyDefaults: SwiftDefaults {
     dynamic var value2: String = "10"
     dynamic var value3: Int = 1
     dynamic var value4: Person? = nil
+    dynamic var value5: Date? = nil
 }
 
--- Pod/Classes/SwiftDefaults.swift
+++ Pod/Classes/SwiftDefaults.swift
@@ -1,7 +1,7 @@
 import Foundation
 
-public class SwiftDefaults: NSObject {
-    let userDefaults = NSUserDefaults.standardUserDefaults()
+open class SwiftDefaults: NSObject {
+    let userDefaults = UserDefaults.standard
     
     public override init() {
         super.init()
@@ -17,12 +17,13 @@ public class SwiftDefaults: NSObject {
 }
 
 extension SwiftDefaults {
-    override public func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+    override open func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         if let keyPath = keyPath {
-            if let value = change?["new"] where !(value is NSNull) {
-                userDefaults.setObject(value is NSCoding ? NSKeyedArchiver.archivedDataWithRootObject(value) : value, forKey: storeKey(keyPath))
+            let key = NSKeyValueChangeKey(rawValue: "new")
+            if let value = change?[key], !(value is NSNull) {
+                userDefaults.set(value is NSCoding ? NSKeyedArchiver.archivedData(withRootObject: value) : value, forKey: storeKey(keyPath))
             }else{
-                userDefaults.removeObjectForKey(storeKey(keyPath))
+                userDefaults.removeObject(forKey: storeKey(keyPath))
             }
 
             userDefaults.synchronize()
@@ -31,23 +32,23 @@ extension SwiftDefaults {
 }
 
 extension SwiftDefaults {
-    private func storeKey(propertyName: String) -> String{
-        return "\(self.dynamicType)_\(propertyName)"
+    fileprivate func storeKey(_ propertyName: String) -> String{
+        return "\(type(of: self))_\(propertyName)"
     }
     
-    private func registerDefaults() {
+    fileprivate func registerDefaults() {
         let dic = propertyNames.reduce([String:AnyObject]()) { (dic, key) -> [String:AnyObject] in
             var mutableDic = dic
-            mutableDic[storeKey(key)] = valueForKey(key)
+            mutableDic[storeKey(key)] = value(forKey: key) as AnyObject?
             return mutableDic
         }
-        userDefaults.registerDefaults(dic)
+        userDefaults.register(defaults: dic)
     }
     
-    private func setupProperty() {
+    fileprivate func setupProperty() {
         propertyNames.forEach {
-            let value = userDefaults.objectForKey(storeKey($0))
-            if let data = value as? NSData, decodedValue = NSKeyedUnarchiver.unarchiveObjectWithData(data){
+            let value = userDefaults.object(forKey: storeKey($0))
+            if let data = value as? Data, let decodedValue = NSKeyedUnarchiver.unarchiveObject(with: data){
                 setValue(decodedValue, forKey: $0)
             }else{
                 setValue(value, forKey: $0)
@@ -55,19 +56,19 @@ extension SwiftDefaults {
         }
     }
     
-    private func addObserver() {
+    fileprivate func addObserver() {
         propertyNames.forEach {
-            addObserver(self, forKeyPath: $0, options: .New, context: nil)
+            addObserver(self, forKeyPath: $0, options: .new, context: nil)
         }
     }
     
-    private func removeObserver() {
+    fileprivate func removeObserver() {
         propertyNames.forEach {
             removeObserver(self, forKeyPath: $0)
         }
     }
     
-    private var propertyNames: [String] {
+    fileprivate var propertyNames: [String] {
         return Mirror(reflecting: self).children.flatMap { $0.label }
     }
 }
